94acd061b56e7048775715764917b401c12ad740
Ayush <ayushprasad481@gmail.com>
2025-11-24 03:03:26 +0530

    Add llm.py to implement Hugging Face text generation functionality

---
+++ app/llm.py
@@ -0,0 +1,13 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+
+from dotenv import load_dotenv
+
+
+load_dotenv()
+llm = HuggingFaceEndpoint(
+    repo_id="mistralai/Mistral-7B-Instruct-v0.2",
+    task="text-generation",
+    temperature=0,
+    max_new_tokens=300,
+)
+chat = ChatHuggingFace(llm=llm)
